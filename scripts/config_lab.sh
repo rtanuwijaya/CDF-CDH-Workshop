f64d3ee0678cb365a0e20a55f74e3582457fbbac
rtanuwijaya <50467630+rtanuwijaya@users.noreply.github.com>
2019-10-11 10:10:59 +0700

    Update config_lab.sh

--- scripts/config_lab.sh
+++ scripts/config_lab.sh
@@ -2,7 +2,7 @@
 cd
 echo Installing Lab dependencies
 sudo yum install unzip
-wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip --no-check-certificate
 unzip stanford-corenlp-full-2018-10-05.zip
 wget  http://central.maven.org/maven2/org/apache/kudu/kudu-spark2_2.11/1.9.0/kudu-spark2_2.11-1.9.0.jar
 wget https://raw.githubusercontent.com/swordsmanliu/SparkStreamingHbase/master/lib/spark-core_2.11-1.5.2.logging.jar
